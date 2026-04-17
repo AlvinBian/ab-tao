ec8d5aea33f25df1e48a688f661c3c1eb1584bf9
Alvin Bian <alvin.bian@kkday.com>
2026-04-18 01:21:21 +0800

    feat(dotfiles): 補全增強（四級匹配 + 快取 + completer chain）+ 按鍵（Home/End/Delete + edit-command-line + WORDCHARS）+ starship fallback

--- apps/dotfiles/zsh/.zshrc.d/conf/20-keys.zsh
+++ apps/dotfiles/zsh/.zshrc.d/conf/20-keys.zsh
@@ -20,3 +20,18 @@ bindkey '^[[B'    history-search-forward   # ↓ 前綴搜尋歷史
 bindkey '^U'      backward-kill-line
 bindkey '^K'      kill-line
 bindkey '^W'      backward-kill-word
+
+# Home / End / Delete（terminfo 可攜式，跨終端機相容）
+if (( ${+terminfo} )); then
+  [[ -n "${terminfo[khome]}" ]] && bindkey "${terminfo[khome]}" beginning-of-line
+  [[ -n "${terminfo[kend]}" ]]  && bindkey "${terminfo[kend]}" end-of-line
+  [[ -n "${terminfo[kdch1]}" ]] && bindkey "${terminfo[kdch1]}" delete-char
+fi
+
+# 在 $EDITOR 中編輯當前命令（Ctrl+X Ctrl+E）
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^X^E' edit-command-line
+
+# 路徑分隔：移除 / 讓 Ctrl+W / Option+Backspace 在路徑分隔符停止
+WORDCHARS=${WORDCHARS//[\/]}
